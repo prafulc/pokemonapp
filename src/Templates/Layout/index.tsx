424e01eebfce85385bf8b5f6f113c9c69207d218
prafulc <pchandekar10@gmail.com>
2022-05-04 10:18:55 +0530

    Layout setup

--- src/Templates/Layout/index.tsx
+++ src/Templates/Layout/index.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+
 import ErrorBoundary from "../../Organism/ErrorBoundary/ErrorBoundary";
+import style from "./index.module.scss";
 
 const Layout = (): JSX.Element => {
   return (
-    <ErrorBoundary>
-        Layout
-      <Outlet />
-    </ErrorBoundary>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"
+            alt="pokemon"
+            loading="lazy"
+            className={style.logo}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <ErrorBoundary>
+            <Outlet />
+          </ErrorBoundary>
+        </div>
+      </div>
+    </div>
   );
 };
 
